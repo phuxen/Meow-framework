a1f6d829184d192b3ab45ab8aa514fe0aab211f5
Paul Huxen <phuxen@gmail.com>
2013-03-06 20:28:04 +0000

    404 errors updated

--- web_assets/js/meow.js
+++ web_assets/js/meow.js
@@ -107,6 +107,31 @@ $(document).ready(function($){
 	/* SLider fix */
 	$(".iosSlider").css("height","500px");
 	
+	/* 
+	addEventListener("load", function() 
+	{ 
+		setTimeout(updateLayout, 0); 
+	}, false); 
+	
+	var currentWidth = 0; 
+	
+	function updateLayout() 
+	{ 
+		if (window.innerWidth != currentWidth) 
+	{ 
+	currentWidth = window.innerWidth; 
+	
+	var orient = currentWidth == 320 ? "profile" : "landscape"; 
+	document.body.setAttribute("orient", orient); 
+	setTimeout(function() 
+	{ 
+	window.scrollTo(0, 1); 
+	}, 100); 
+	} 
+	} */
+   
+   setInterval(updateLayout, 100); 
+	
 	//Add a listener to check the size of the document when you load
 	$(document).resize(checkSize);
 	checkSize();
